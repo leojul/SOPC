6f9cd2840f66c1d79819ac0bef9b3bf11868aead
jules <jules.diard@gmail.com>
2019-11-15 15:48:06 +0100

    Heuristique ajouté et elle tournegit add SOP/src/sop/*!

--- SOP/src/sop/Parser.java
+++ SOP/src/sop/Parser.java
@@ -9,6 +9,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -108,6 +109,33 @@ public class Parser {
             noDoublon[tabSol[i]]=-1;
         }
         System.out.println("result= "+result);
+    }
+    
+    public void checker(ArrayList<Integer> sol) throws ConstrainedDisrespect{
+        int[] tab = new int[n];
+        for (int i = 0; i < n; i++) {
+            tab[i]=sol.get(i);
+        }
+       int result=0;
+       int [] noDoublon =new int [tab.length];   //tableau pour vérifier que l'on ne passe pas plusieur fois par un même sommet
+       if(tab.length!=n){
+           throw new ConstrainedDisrespect("the lenght of the solution should be equal to the number of vertices");
+       }
+        for (int i = 0; i < tab.length; i++) {
+            noDoublon[i]=i;
+        }
+        for (int i = 0; i < tab.length-1; i++) {
+            if(matrix[tab[i]][tab[i+1]]==-1){
+                result=-1;
+                throw new ConstrainedDisrespect("contraint precedence");
+            }else if (noDoublon[tab[i]]==-1){
+                throw new ConstrainedDisrespect("do not go through the same vertice more than once");
+            }else{
+                result+=matrix[tab[i]][tab[i+1]];
+            }
+            noDoublon[tab[i]]=-1;
+        }
+        System.out.println("result heuristique= "+result);
     }
 
     private static class ConstrainedDisrespect extends Exception {
